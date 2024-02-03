1b5fef24eaafd08ea71cd1d03d9b9b32473327d5
manmohansingh325 <manmohankumar325@gmail.com>
2024-02-03 20:53:43 +0530

    app js modified

---
+++ app.js
@@ -0,0 +1,57 @@
+const apiurl = "https://api.github.com/users/";
+const main = document.querySelector("#main");
+
+const getUser = async (username) => {
+  const response = await fetch(apiurl + username);
+  const data = await response.json();
+  console.log(data);
+  const card = `<div class="card">
+            <div class="img">
+                <img src="${data.avatar_url}" alt="profile-photo" class="avatar">
+            </div>
+
+  <div class="user-info">
+      <h2>${data.name}</h2>
+      <p>${data.bio}</p>
+
+      <ul class="info">
+          <li>${data.followers}<strong> Followers</strong></li>
+          <li>${data.following}<strong> Following</strong></li>
+          <li>${data.public_repos}<strong> Repos</strong></li>
+      </ul>
+      
+      <div id="repos">
+      </div>
+     
+  </div>
+</div>`;
+  main.innerHTML = card;
+  getRepos(username);
+};
+
+getUser("codewithmanmohan");
+const getRepos = async (username) => {
+  const repos = document.querySelector("#repos");
+  const response = await fetch(apiurl + username + "/repos");
+  const data = await response.json();
+  data.forEach((item) => {
+    const elem = document.createElement("a");
+    elem.classList.add("repo");
+    elem.href = item.html_url;
+    elem.innerText = item.name;
+    elem.target = "_blank";
+    repos.appendChild(elem);
+  });
+};
+
+const formSubmit = () => {
+  const searchbox = document.querySelector("#search");
+  if (searchbox != "") {
+    getUser(searchbox.value);
+  }
+  return false;
+};
+
+/* <a href="#" target="_blank" class="repo">Repo1</a>
+          <a href="#" target="_blank" class="repo">Repo2</a>
+          <a href="#" target="_blank" class="repo">Repo3</a> */
